b15769ccdde0f7dba80c7b6f225937f3a199bb27
paxington181 <tsandrus@rogers.com>
2025-04-25 16:25:54 -0400

    Working on coding a keyboard generator

--- main.py
+++ main.py
@@ -4,21 +4,36 @@ from tkinter import ttk
 
 import random
 
+back_ground = "dimgrey"
+word_definition = "Placeholder text that will be later filled with a definition once I go from rigging my UI to making the UI actually work"
+
+keyboard1 = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
+keyboard2 = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
+keyboard3 = ["z", "x", "c", "v", "b", "n", "m"]
 
 main_window = tk.Tk()
+main_window.geometry("800x400")
+main_window.configure(bg = back_ground)
 main_window.title("Word Explorer")
 
 guess_box = Listbox(main_window)
 guess_box.grid(row = 0, column = 1)
 
-word_definition = Text(main_window)
+word_definition_text = Text(main_window, height = 4, width = 30, bg = back_ground)
+word_definition_text.grid(row = 0, column = 2)
+word_definition_text.insert(tk.END, word_definition)
 
-Label(main_window, text = "Guess").grid(row = 1, column = 2)
+Label(main_window, bg = back_ground, text = "Guess").grid(row = 1, column = 2)
 word_entry = Entry(main_window)
 word_entry.grid(row = 1, column = 3)
 
+submit_button = tk.Button(main_window, bg = back_ground, text = "Guess word", width = 10)
+submit_button.grid(row = 2, column = 2)
 
 
-
+for key in keyboard1:
+    
+    _button = tk.Button(main_window, bg = back_ground, text = key, width = 1)
+    _button.grid(row = , column = )
 
 main_window.mainloop()
