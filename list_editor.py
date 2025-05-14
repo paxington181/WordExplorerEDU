4945ad10f102fd4c9e1e15ce6a76b079890b06e5
paxington181 <tsandrus@rogers.com>
2025-05-13 22:08:58 -0400

    Minor updates

--- list_editor.py
+++ list_editor.py
@@ -1,7 +1,13 @@
+import tkinter as tk
+from tkinter import *
+
 def main():
     word_list = []
     definition_list = []
-    
+
+#Reads the wordlist file, removes comments and then adds the words and definitions to their lists without the prefixes.    
+
+
     with open("wordlist.txt") as file:
         unmodified_wordlist = file.read().splitlines()
         for line in unmodified_wordlist:
@@ -11,7 +17,12 @@ def main():
                 word_list.append(line[2:])
             elif line.startswith("d."):
                 definition_list.append(line[2:])
-    
+
+#Reads through the list of words and definitions and gives ordered boxes that are easier for users that are less comfortable with computers to manipulate the word list.
+
     print(word_list)
+    print(len(word_list))   
     print(definition_list)
+    print(len(definition_list))
+
 main()
