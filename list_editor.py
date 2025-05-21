2b2a7db37175c5aac3a8a9ceebc5d7d7c8fd2fd1
paxington181 <tsandrus@rogers.com>
2025-05-21 16:04:22 -0400

    Adjustments made to make adding words cleaner

--- list_editor.py
+++ list_editor.py
@@ -1,13 +1,15 @@
+#Imports tkinterface to be able to build the GUI
 import tkinter as tk
 from tkinter import *
+
+#Variables that are needed across the entire program
 word_list = []
 definition_list = []
 
+#The main function that builds the list for the GUI assembly
 def main():
 
 #Reads the wordlist file, removes comments and then adds the words and definitions to their lists without the prefixes.    
-
-
     with open("wordlist.txt") as file:
         unmodified_wordlist = file.read().splitlines()
         for line in unmodified_wordlist:
@@ -20,7 +22,25 @@ def main():
 
 main()
 
+#Functions that are called when the buttons are pressed
+def update_line():
+    pass
+
+def update_all():
+    pass
+
 def remove_line(word_entry):
+    pass
+
+def remove_all():
+    pass
+
+
+def save_line(word_entry):
+    pass
+
+#Removes the current wordlist, and replaces it with the default wordlist
+def reset_default():
     pass
 
 #Builds the main window
@@ -30,7 +50,7 @@ main_window.title("Word Explorer")
 
 #Adds buttons to makes changes to the whole list with a single click
 mb_frame = LabelFrame(main_window)
-mb_frame.grid(row = 0, column = 0)
+mb_frame.pack()
 update_all = Button(mb_frame, text = "Update All")
 update_all.grid(row = 0, column = 0)
 remove_all = Button(mb_frame, text = "Remove All")
@@ -38,14 +58,26 @@ remove_all.grid(row = 0, column = 1)
 reset_default = Button(mb_frame, text = "Reset to Default")
 reset_default.grid(row = 0, column = 2)
 
-#Generates the word list, definition list, and buttons to modify entries
+#Entry fields for adding words and definitions to the list
+add_frame = LabelFrame(main_window, text = "Add Word")
+add_frame.pack()
+word_entry = Entry(add_frame)
+word_entry.grid(row = 0, column = 0)
+definition_entry = Entry(add_frame, width = 100)
+definition_entry.grid(row = 0, column = 1)
+add_word = Button(add_frame, text = "Add Word")
+add_word.grid(row = 0, column = 2)
+clear_entry = Button(add_frame, text = "Clear Entries")
+clear_entry.grid(row = 0, column = 3)
+
+#Generates boxes for the word list, definition list, and buttons to modify entries
 word_frame = LabelFrame(main_window, text = "Word List")
-word_frame.grid(row = 1, column = 0)
+word_frame.pack()
 for i in range(0, len(word_list)):
     word_box = Entry(word_frame)
     word_box.insert(0, word_list[i])
     word_box.grid(row = i + 1, column = 0)
-    definition_box = Entry(word_frame)
+    definition_box = Entry(word_frame, width = 100)
     definition_box.insert(0, definition_list[i])
     definition_box.grid(row = i + 1, column = 1)
     update_line = Button(word_frame, text = "Update")
@@ -53,15 +85,5 @@ for i in range(0, len(word_list)):
     remove_line = Button(word_frame, text = "Remove")
     remove_line.grid(row = i + 1, column = 3)
 
-#Adds 10 empty rows of entry boxes to add to the word list
-for i in range(0, 10):
-    word_box = Entry(word_frame)
-    word_box.grid(row = i + (len(word_list) + 1), column = 0)
-    definition_box = Entry(word_frame)
-    definition_box.grid(row = i + (len(word_list) + 1), column = 1)
-    save_line = Button(word_frame, text = "Save")
-    save_line.grid(row = i + (len(word_list) + 1), column = 2)
-    remove_line = Button(word_frame, text = "Remove")
-    remove_line.grid(row = i + (len(word_list) + 1), column = 3)
 
 main_window.mainloop()
