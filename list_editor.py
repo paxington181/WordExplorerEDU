f865ac81c65a0a762aab2598a35d5b4417b1bdc9
paxington181 <tsandrus@rogers.com>
2025-06-15 20:42:08 -0400

    Layout adjustments for list_editor

--- list_editor.py
+++ list_editor.py
@@ -38,19 +38,10 @@ main_window = tk.Tk()
 main_window.configure(bg = "lightgrey")
 main_window.title("Word Explorer")
 
-#Adds buttons to makes changes to the whole list with a single click
-mb_frame = LabelFrame(main_window)
-mb_frame.pack()
-update_all = Button(mb_frame, text = "Update All")
-update_all.grid(row = 0, column = 0)
-remove_all = Button(mb_frame, text = "Remove All")
-remove_all.grid(row = 0, column = 1)
-reset_default = Button(mb_frame, text = "Reset to Default")
-reset_default.grid(row = 0, column = 2)
 
 #Entry fields for adding words and definitions to the list
 add_frame = LabelFrame(main_window, text = "Add Word", padx = 10)
-add_frame.pack()
+add_frame.pack(pady = 10)
 word_entry = Entry(add_frame)
 word_entry.grid(row = 0, column = 0)
 definition_entry = Entry(add_frame, width = 100)
@@ -62,7 +53,7 @@ clear_entry.grid(row = 0, column = 3)
 
 #Generates boxes for the word list, definition list, and buttons to modify entries
 word_frame = LabelFrame(main_window, text = "Word List")
-word_frame.pack()
+word_frame.pack(side = LEFT, pady = 10)
 i = 0
 for key, value in wordlist.items():
     word_box = Entry(word_frame)
@@ -71,12 +62,21 @@ for key, value in wordlist.items():
     definition_box = Entry(word_frame, width = 100)
     definition_box.insert(0, value)
     definition_box.grid(row = i, column = 1)
-    update_line = Button(word_frame, text = "Update")
-    update_line.grid(row = i, column = 2)
+#    update_line = Button(word_frame, text = "Update")  - to restore button, remove line column needs updated to 3 again
+#    update_line.grid(row = i, column = 2)
     remove_line = Button(word_frame, text = "Remove")
-    remove_line.grid(row = i, column = 3)
+    remove_line.grid(row = i, column = 2)
     i += 1
     
+#Adds buttons to makes changes to the whole list with a single click
+mb_frame = LabelFrame(main_window)
+mb_frame.pack(side = RIGHT, pady = 10)
+update_all = Button(mb_frame, text = "Save Changes")
+update_all.grid(row = 0, column = 0, pady = 20)
+remove_all = Button(mb_frame, text = "Remove All Words")
+remove_all.grid(row = 1, column = 0, pady = 20)
+reset_default = Button(mb_frame, text = "Reset List to Default")
+reset_default.grid(row = 2, column = 0, pady = 20)
 
 
 main_window.mainloop()
